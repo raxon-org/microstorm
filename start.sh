906bedbf49fa2c6bad02b1f6fa6e31b629db513c
Remco van der Velde <remco@universeorange.com>
2026-01-26 19:29:30 +0100

    pp

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "TERM=xterm-256color" >> ~/.bashrc
 echo "composer install -n" >> ~/.bashrc
-echo "frankenphp php-server -r Public/ &" >> ~/.bashrc
+echo "frankenphp php-server -r Public/ --domain=workandtravel.world &" >> ~/.bashrc
