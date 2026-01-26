33a4278678a06213217721d33d76c0a95a858375
Remco van der Velde <remco@universeorange.com>
2026-01-26 20:14:00 +0100

    pp

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo "TERM=xterm-256color" >> ~/.bashrc
 echo "composer install -n" >> ~/.bashrc
+echo "sysctl -w net.core.rmem_max=7500000"
+echo "sysctl -w net.core.wmem_max=7500000"
 echo "frankenphp php-server -r Public/ --domain=workandtravel.world &" >> ~/.bashrc
