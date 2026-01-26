17acf095950b6e42f1ccc58a2004426e290bda19
Remco van der Velde <remco@universeorange.com>
2026-01-26 14:33:34 +0100

    pp

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "TERM=xterm-256color" >> ~/.bashrc
 echo "composer install -n" >> ~/.bashrc
-echo "frankenphp php-server -c Caddyfile -r Public/ &" >> ~/.bashrc
+echo "frankenphp php-server -r Public/ &" >> ~/.bashrc
