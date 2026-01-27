e820d2e50347da21c2b27bc8f0df11bc5b670ed2
Remco van der Velde <remco@universeorange.com>
2026-01-27 12:33:04 +0100

    pp

--- Public/app.php
+++ Public/app.php
@@ -2,7 +2,14 @@
 if(!defined('MICROSTORM')){
     die( 'Forbidden');
 }
-$dir_application_framework = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'Framework' . DIRECTORY_SEPARATOR;
+$dir_application_framework = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Framework' . DIRECTORY_SEPARATOR;
 require_once $dir_application_framework . 'Boot.php';
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$app = new Boot();
+var_dump($app);
+die;
+
+
+
 return Boot::app();
