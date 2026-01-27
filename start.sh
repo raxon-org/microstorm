604da355c8509b98a13386261bbdd30a9db36968
Remco van der Velde <remco@universeorange.com>
2026-01-27 11:15:11 +0100

    pp

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ echo "TERM=xterm-256color" >> ~/.bashrc
 echo "composer install -n" >> ~/.bashrc
 #echo "sysctl -w net.core.rmem_max=7500000" in root system for http3
 #echo "sysctl -w net.core.wmem_max=7500000" in root system for http3
-echo "frankenphp php-server -r Public/ --domain=localhost &" >> ~/.bashrc
+echo "frankenphp run --config /Application/Caddyfile &" >> ~/.bashrc
