462d9e534f2d0227979eb661c055a72a631e3ec9
Remco van der Velde <remco@universeorange.com>
2026-01-27 16:32:27 +0100

    pp

--- src/Ramdisk.php
+++ src/Ramdisk.php
@@ -1,6 +1,15 @@
 <?php
 namespace Microstorm;
+
 require_once __DIR__ . '/Dir.php';
+
 use Microstorm\Dir;
+
 $dir_tmp = '/tmp/raxon/org/Plugin/';
-Dir::create($dir_tmp, Dir::CHMOD);
+if(!Dir::is($dir_tmp)){
+    Dir::create($dir_tmp, Dir::CHMOD);
+    $dir_plugin = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Plugin' . DIRECTORY_SEPARATOR;
+    $dir = new Dir();
+    $read = $dir->read($dir_plugin);
+    d($read);
+}
