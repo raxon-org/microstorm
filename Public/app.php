1c356eff7381a29350f37716efbaf74354ac518e
Remco van der Velde <remco@universeorange.com>
2026-01-27 16:27:47 +0100

    pp

--- Public/app.php
+++ Public/app.php
@@ -3,5 +3,6 @@ if(!defined('MICROSTORM')){
     die( 'Forbidden');
 }
 $autoload = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-require_once 'debug.php';
+require_once '../src/Debug.php';
+require_once '../src/Ramdisk.php';
 return new Microstorm\Boot($autoload);
---
+++ src/Ramdisk.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/Dir.php';
+$dir_tmp = '/tmp/raxon/org/Plugin/';
+Dir::create($dir_tmp, Dir::CHMOD);
