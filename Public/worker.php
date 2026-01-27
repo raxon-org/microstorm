4a18444e99b3a0ff4af1f892e73dd6842062bdc7
Remco van der Velde <remco@universeorange.com>
2026-01-27 11:25:17 +0100

    pp

--- Public/worker.php
+++ Public/worker.php
@@ -1,3 +1,27 @@
 <?php
-echo '<h1>Worker</h1>';
-frankenphp_handle_request();
+$app = (object) [
+    'time' => [
+        'start' => microtime(true)
+    ]
+];
+$handler = static function () use ($app) {
+    $app->time->current = microtime(true);
+    $app->time->duration = $app->time->current - $app->time->start;
+    // Called when a request is received,
+    // superglobals, php://input and the like are reset
+
+    var_dump($app);
+    var_dump($_GET);
+    var_dump($_POST);
+    var_dump($_COOKIE);
+    var_dump($_FILES);
+    var_dump($_SERVER);
+};
+$count = 0;
+$max = 5;
+while (frankenphp_handle_request($handler)) {
+    $count++;
+    if($count >= $max){
+        break;
+    }
+}
