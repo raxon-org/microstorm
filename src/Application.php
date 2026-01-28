e83f27ed6a12c71eb5d160a25f9c3573b71bebf6
Remco van der Velde <remco@universeorange.com>
2026-01-28 21:11:05 +0100

    init

--- src/Application.php
+++ src/Application.php
@@ -2,18 +2,17 @@
 namespace Microstorm;
 
 use Exception;
-//use Plugin;
+use Module\Data;
+use Plugin;
 
 class Application {
-    /*
+
     use Plugin\Autoload;
     use Plugin\Config;
     use Plugin\Data;
     use Plugin\Destination;
-    use Plugin\Init;
     use Plugin\Request;
     use Plugin\Route;
-    */
 
     protected ?object $autoload = null;
     protected ?object $config = null;
@@ -23,13 +22,47 @@ class Application {
      * @throws Exception
      */
     public function __construct(null|object $autoload=null, null|Data $config = null, null|Data $data = null) {
-        /*
         $this->autoload($autoload);
         $this->config($config);
         $this->data($data);
-        $this->init(__DIR__);
-        */
+        $this->init();
+
         echo 'Hello Microstorm!' . PHP_EOL;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function init($dir=__DIR__): void
+    {
+        $config = $this->config();
+        if ($config === null) {
+            $config = new Data((object)[
+                'time' => (object)[
+                    'start' => MICROSTORM,
+                    'current' => null,
+                    'duration' => null
+                ],
+                'directory' => (object)[
+                    'temp' => '/tmp/raxon/org/',
+                    'root' => dirname($dir) . DIRECTORY_SEPARATOR,
+                    'controller' => dirname($dir) . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR,
+                    'data' => dirname($dir) . DIRECTORY_SEPARATOR . 'Data' . DIRECTORY_SEPARATOR,
+                    'public' => dirname($dir) . DIRECTORY_SEPARATOR . 'Public' . DIRECTORY_SEPARATOR,
+                    'source' => dirname($dir) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR,
+                    'vendor' => dirname($dir) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR,
+                    'view' => dirname($dir) . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR,
+                ]
+            ]);
+            if ($config->get('directory.root') === $config->get('directory.temp')) {
+                throw new Exception('$dir argument is invalid.');
+            }
+            $this->config($config);
+        }
+        $data = $this->data();
+        if ($data === null) {
+            $this->data(new Data());
+        }
     }
 
     /**
