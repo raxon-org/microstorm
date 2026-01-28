aa52a27295d641106e11b381c9503a8e682c6a99
Remco van der Velde <remco@universeorange.com>
2026-01-28 02:40:18 +0100

    pp

--- Plugin/Init.php
+++ Plugin/Init.php
@@ -9,7 +9,7 @@ trait Init {
     /**
      * @throws Exception
      */
-    private function init(): void
+    private function init($dir=__DIR__): void
     {
         $config = $this->config();
         if($config === null) {
@@ -21,11 +21,11 @@ trait Init {
                 ],
                 'directory' => (object)[
                     'temp' => '/tmp/raxon/org/',
-                    'root' => dirname(__DIR__) . DIRECTORY_SEPARATOR,
-                    'data' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Data' . DIRECTORY_SEPARATOR,
-                    'public' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Public' . DIRECTORY_SEPARATOR,
-                    'source' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR,
-                    'vendor' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR,
+                    'root' => dirname($dir) . DIRECTORY_SEPARATOR,
+                    'data' => dirname($dir) . DIRECTORY_SEPARATOR . 'Data' . DIRECTORY_SEPARATOR,
+                    'public' => dirname($dir) . DIRECTORY_SEPARATOR . 'Public' . DIRECTORY_SEPARATOR,
+                    'source' => dirname($dir) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR,
+                    'vendor' => dirname($dir) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR,
                 ]
             ]);
             $this->config($config);
