ff750f97b3d6ec1742b1ee5720a1eac25b14539d
Remco van der Velde <remco@universeorange.com>
2026-01-29 17:16:23 +0100

    k

--- src/Cli/Task.php
+++ src/Cli/Task.php
@@ -2,7 +2,9 @@
 namespace Microstorm\Cli;
 
 use Exception;
+use Module\Core;
 use Module\Data;
+use Module\Dir;
 use Module\File;
 use Plugin;
 
@@ -29,8 +31,11 @@ class Task {
                 //list all tasks
                 break;
             case 'run':
+                echo $this->task_run($config);
                 return 'Task run...' . PHP_EOL;
                 break;
+            case 'monitor':
+                return $this->task_monitor($config);
             case 'info':
             default:
                 $info = [];
@@ -43,5 +48,141 @@ class Task {
                 return implode(PHP_EOL, $info) . PHP_EOL;
         }
         return '';
+    }
+
+    public function task_get(Data $config): array
+    {
+        return [
+            'uuid' => Core::uuid()
+        ];
+    }
+
+    private function task_run(Data $config): void
+    {
+        $time_start = time();
+        while(true) {
+            $is_busy = false;
+            $record = $this->task_get($config);
+            if (array_key_exists('uuid', $record)) {
+                $patch = [
+                    'uuid' => $record['uuid'],
+                    'status' => Status::IN_PROGRESS,
+                ];
+                //status IN_PROGRESS after 120 mins it should be set to ERROR
+                $is_busy = true;
+            }
+            if ($is_busy === false) {
+                sleep(5);
+            } else {
+                $dir_package = $config->get('directory.temp') . 'Task' . DIRECTORY_SEPARATOR;
+                $dir_stdout = $dir_package . 'stdout' . DIRECTORY_SEPARATOR;
+                $dir_stderr = $dir_package . 'stderr' . DIRECTORY_SEPARATOR;
+                Dir::create($dir_stdout, Dir::CHMOD);
+                Dir::create($dir_stderr, Dir::CHMOD);
+                $process_list = [];
+                if (array_key_exists('uuid', $record)) {
+                    if (
+                        array_key_exists('command', $record)
+                    ) {
+                        $url_stdout = $dir_stdout . $record['uuid'];
+                        $url_stderr = $dir_stderr . $record['uuid'];
+                        foreach ($record['node']['command'] as $nr => $command) {
+                            $command = 'nohup ' . $command . ' >> ' . $url_stdout . ' 2>> ' . $url_stderr . ' &  echo $!';
+                            exec($command, $output, $code);
+                            $proc_id = trim($output[0]);
+                            $process_list[] = $proc_id;
+                        }
+                        $command = 'nohup microstorm task monitor -task.uuid=' . $record['uuid'];
+                        foreach ($process_list as $proc_id) {
+                            $command .= ' -process[]=' . $proc_id;
+                        }
+                        exec($command, $output, $code);
+                    }
+                }
+            }
+            $time_current = time();
+            if ($time_current - $time_start > 60) { // 1 minute time-out
+                //timeout cron every minute
+                break;
+            }
+        }
+    }
+
+    private function task_monitor(Data $config): string
+    {
+        $flags = $this->flags();
+        $options = $this->options();
+        $record = $this->task_get($config);
+        $dir_package = $config->config('directory.temp') . 'Task' . DIRECTORY_SEPARATOR;
+        $dir_stdout = $dir_package . 'stdout' . DIRECTORY_SEPARATOR;
+        $dir_stderr = $dir_package . 'stderr' . DIRECTORY_SEPARATOR;
+        $url_stdout = $dir_stdout . $record['uuid'];
+        $url_stderr = $dir_stderr . $record['uuid'];
+        $time_start = time();
+        while(true){
+            $process_active = [];
+            foreach($options->process as $proc_id) {
+                $command = 'ps -p ' . $proc_id;
+                exec($command, $output, $code);
+                $process_active[] = $code;
+            }
+            if(!in_array(0, $process_active)) {
+                //task completed
+                $patch = [
+                    'uuid' => $record['uuid'],
+                    'status' => Status::COMPLETED,
+                ];
+                if(File::exist($url_stdout)){
+                    $stdout = File::read($url_stdout, ['return' => File::ARRAY]);
+                    $patch['output'] = $stdout;
+                    File::delete($url_stdout);
+                }
+                if(File::exist($url_stderr)){
+                    $stderr = File::read($url_stderr, ['return' => File::ARRAY]);
+                    $patch['notification'] = $stderr;
+                    File::delete($url_stderr);
+                }
+                $record = array_merge($record, $patch);
+                break;
+            }
+            //task is running
+            usleep(0.5 * 1000 * 1000);
+            $time_current = time();
+            if($time_current - $time_start > 120 * 60 * 60){ // 2 hours time-out
+                //timeout
+                $patch = [
+                    'id' => $record['id'],
+                    'status' => Status::ERROR,
+                ];
+                if(File::exist($url_stdout)){
+                    $stdout = File::read($url_stdout, ['return' => File::ARRAY]);
+                    $patch['output'] = $stdout;
+                    File::delete($url_stdout);
+                }
+                if(File::exist($url_stderr)){
+                    $stderr = File::read($url_stderr, ['return' => File::ARRAY]);
+                    $patch['notification'] = $stderr;
+                    File::delete($url_stderr);
+                }
+                $record = array_merge($record, $patch);
+                break;
+            } else {
+                //updates the task output / notification every half a second.
+                $patch = [
+                    'id' => $record['node']['id'],
+                ];
+                if(File::exist($url_stdout)){
+                    $stdout = File::read($url_stdout, ['return' => File::ARRAY]);
+                    $patch['output'] = $stdout;
+                }
+                if(File::exist($url_stderr)){
+                    $stderr = File::read($url_stderr, ['return' => File::ARRAY]);
+                    $patch['notification'] = $stderr;
+                }
+                $record = array_merge($record, $patch);
+            }
+            d($record);
+        }
+        return 'Task run...' . PHP_EOL;
     }
 }
