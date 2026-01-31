fe86bbb94ea7d674ccaf859caf6ea732372effb3
Remco van der Velde <remco@universeorange.com>
2026-01-31 11:47:22 +0100

    p

--- src/Controller/FileRequest.php
+++ src/Controller/FileRequest.php
@@ -1,12 +1,18 @@
 <?php
 namespace Microstorm\Controller;
 
+use Module\Data;
+use Module\File;
+
 class FileRequest {
 
-    public function get($config): string
+    public function get(Data $config): string
     {
-        ddd($config);
-
-        return '';
+        $current = $config->get('route.current');
+        if(!$current){
+            return '';
+        }
+        $url = $config->get('directory.source') . $current->get('path');
+        return File::read($url);
     }
 }
