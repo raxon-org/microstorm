e94236210e6e2330dd5cd5553f9fdf48d12af80f
Remco van der Velde <remco@universeorange.com>
2026-01-31 11:58:18 +0100

    p

--- src/Controller/FileRequest.php
+++ src/Controller/FileRequest.php
@@ -13,6 +13,23 @@ class FileRequest {
             return '';
         }
         $url = str_replace('../', '', $config->get('directory.source') . 'Public/' . $current->get('path'));
+        if(File::exists($url) && !headers_sent()){
+            $extension = File::extension($url);
+            switch ($extension){
+                case 'css': $contentType = 'text/css'; break;
+                case 'json': $contentType = 'application/json'; break;
+                case 'js': $contentType = 'application/javascript'; break;
+                default: $contentType = 'text/plain';
+            }
+            $etag = sha1($url);
+            $gm = gmdate('D, d M Y H:i:s T', File::mtime($url));
+            header('HTTP/1.1 200 OK');
+            header('Last-Modified: '. $gm);
+            header('ETag: ' . $etag . '-' . $gm);
+            header('Cache-Control: public');
+            header('Content-Type: ' . $contentType);
+
+        }
         return File::read($url);
     }
 }
