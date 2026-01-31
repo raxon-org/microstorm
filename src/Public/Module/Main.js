3e6e2ba14569644bea06bd395f88fe076c1e2203
Remco van der Velde <remco@universeorange.com>
2026-01-31 17:20:16 +0100

    p

--- src/Public/Module/Main.js
+++ src/Public/Module/Main.js
@@ -45,10 +45,8 @@ main.line_count = (editor) => {
 main.column_count = (editor, line_nr) => {
     const text = editor.innerText.split("\n");
     let index;
-    console.log('line nr:' + line_nr);
-    console.log(text);
     for(index=0; index < text.length; index++){
-        if(index === line_nr){
+        if(index === (line_nr - 1)){
             return text[index].length;
         }
     }
