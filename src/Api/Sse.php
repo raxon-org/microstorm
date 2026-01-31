815e54f7f5758cf37d21499d8c249407126a3c9f
Remco van der Velde <remco@universeorange.com>
2026-01-31 16:18:43 +0100

    p

--- src/Api/Sse.php
+++ src/Api/Sse.php
@@ -2,6 +2,7 @@
 namespace Microstorm\Api;
 
 use Exception;
+use Module\Core;
 use Module\Data;
 use Module\File;
 use Plugin;
@@ -14,13 +15,14 @@ class Sse {
      */
     public function main(Data $config): void
     {
-        set_time_limit(2 * 60 * 60);
+        $time_limit = 2 * 60 * 60; //2 hours
+        set_time_limit((int) $time_limit);
         date_default_timezone_set("Europe/Amsterdam");
         header("Cache-Control: no-cache");
         header('Content-Encoding: none');
         header("Content-Type: text/event-stream");
         header("Connection: keep-alive");
-        header("Keep-Alive: timeout=30, max=7200");
+        header("Keep-Alive: timeout=30, max=" . (int) $time_limit);
         header("Transfer-encoding: chunked");
         $this->console_interactive();
         echo ":" . str_repeat("a", 4096) . "\n";
@@ -30,14 +32,25 @@ class Sse {
         $time_start = time();
         while(true){
             $time_current = time();
-            $line = ($time_current - $time_start) . 'seconds' . PHP_EOL;
+            //read command line
+            $url_command = $config->get('directory.temp') . 'Command/Command.json';;
+            if(!File::exists($url_command)){
+                $data = new Data();
+                $data->set('Command.action', 'login');
+            } else {
+                $data = new Data();
+                $data->set('Command.action', 'welcome');
+            }
             echo "id: $id\n";
             echo "event: ping\n";
-            echo 'data: ' . $line;
+            echo 'data: ' . Core::object($data->data('Command'),Core::JSON_LINE);
             echo "\n\n";
             flush();
             $id++;
             sleep(1);
+            if($time_current - $time_start > $time_limit){
+                break;
+            }
         }
     }
 }
