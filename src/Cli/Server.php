296f7ee526c614bf56ca7a66684348d6d0312873
Remco van der Velde <remco@universeorange.com>
2026-02-01 14:59:17 +0100

    pp

---
+++ src/Cli/Server.php
@@ -0,0 +1,35 @@
+<?php
+namespace Microstorm\Cli;
+
+use Exception;
+use Module\Data;
+use Module\File;
+use Plugin;
+
+class Server {
+    use Plugin\Config;
+    use Plugin\Request;
+    use Plugin\Flags;
+    use Plugin\Options;
+    use Plugin\Cron;
+
+    protected ?object $config = null;
+
+    /**
+     * @throws Exception
+     */
+    public function run(Data $config): string
+    {
+        $this->config($config);
+        $url = $this->config->get('directory.temp') . 'Boot/Boot.json';
+        switch($this->request('module')){
+            case 'restart': {
+                ddd('frankenphp restart command');
+            }
+            break;
+            default:
+                throw new Exception('Module not found');
+        }
+        return '';
+    }
+}
